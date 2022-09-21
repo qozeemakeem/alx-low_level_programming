c7961991b1cddf974f293ce90be0883dd8c062fd
qozeemakeem <qozeemakeem@gmail.com>
2022-09-20 18:18:16 -0700

    arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* copy the string - function that copies the string pointed to by src
+* @dest: pointer
+* @src: string
+* @_strcpy: variable
+*/
+char *_strcpy(char *dest, char *src)
+{
